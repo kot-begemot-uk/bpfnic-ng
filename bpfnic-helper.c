a4283f3611d91dced5b40c7446b93552dc413d34
Anton Ivanov <anton.ivanov@cambridgegreys.com>
2023-07-19 09:07:30 +0100

    Full to-from kernel demo
    Signed-off-by: Anton Ivanov <anton.ivanov@cambridgegreys.com>

--- bpfnic-helper.c
+++ bpfnic-helper.c
@@ -21,6 +21,9 @@
 #include <linux/ptr_ring.h>
 #include <linux/bpf.h>
 #include <linux/namei.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/delay.h>
 
 #include "bpfnic_shared.h"
 
@@ -37,8 +40,16 @@
 static bool init_done = false;
 
 static char *to_user_path = "";
-
 static struct bpf_map *to_user;
+
+static char *from_user_path = "";
+static struct bpf_map *from_user;
+
+static bool exiting = false;
+
+static DECLARE_WAIT_QUEUE_HEAD(ringbuf_wait);
+
+static struct task_struct *ringbuf_task;
 
 static void build_ringbuf_entry(struct bpfnic_fdb_entry *entry,
 							struct switchdev_notifier_fdb_info *fdb_info, int event)
@@ -56,7 +67,6 @@ static int bpfnic_switchdev_event(struct notifier_block *unused,
 {
 	struct switchdev_notifier_fdb_info *fdb_info = ptr;
 	struct bpfnic_fdb_entry *fdb_entry;
-
 
 	switch (event) {
 	case SWITCHDEV_FDB_ADD_TO_DEVICE:
@@ -76,51 +86,116 @@ static int bpfnic_switchdev_event(struct notifier_block *unused,
 	return NOTIFY_DONE;
 }
 
+static int emit_switchdev_notification(struct bpfnic_fdb_entry *entry)
+{
+	struct net_device *dev;
+	struct switchdev_notifier_fdb_info fdb_info = {};
+
+	ether_addr_copy((u8*) &fdb_info.addr, (u8*) &entry->addr);
+	dev = dev_get_by_index(&init_net, entry->ifindex);
+	fdb_info.info.dev = dev;
+	fdb_info.vid = entry->vid;
+	fdb_info.added_by_user = entry->added_by_user;
+	fdb_info.offloaded = entry->offloaded;
+	printk(KERN_INFO "event: %i device: %i offloaded: %i\n", entry->event, entry->ifindex, entry->offloaded);
+	return call_switchdev_notifiers(entry->event, dev, &fdb_info.info, NULL);
+}
+
 static struct notifier_block bpfnic_switchdev_notifier = {
 	.notifier_call = bpfnic_switchdev_event,
 };
+
+static int ringbuf_thread(void *unused)
+{
+	struct bpfnic_fdb_entry *entry;
+	u64 size;
+
+	/* Grab one more ref to map so it is not deleted under our feet. */
+	bpf_map_inc(from_user);
+	while (!exiting) {
+        msleep_interruptible(1);
+		int count = 0;
+		while (!exiting) {
+			entry = bpf_ringbuf_fetch_next(from_user, &size);
+			if (!entry)
+				break;
+			emit_switchdev_notification(entry);
+			if (count++ > 10) {
+				printk(KERN_ERR "too many notifications\n");
+				schedule();
+			}
+		}
+	}
+	bpf_map_put(from_user);
+	return 0; 
+}
+
+static void __bpfnic_helper_exit(void);
 
 static __init int bpfnic_helper_init(void)
 {
 	int ret;
 
-	if (strlen(to_user_path) == 0)
+	if ((strlen(to_user_path) == 0) || (strlen(from_user_path) == 0))
 		return -ENODEV;
 
 	to_user = bpf_map_get_path(to_user_path, FMODE_READ | FMODE_WRITE);
+	from_user = bpf_map_get_path(to_user_path, FMODE_READ | FMODE_WRITE);
 
 	if (IS_ERR(to_user)) {
 		printk(KERN_ERR "Failed to get bpfmap, ERR %ld", PTR_ERR(to_user));
 		return PTR_ERR(to_user);
-	} else {
-		printk(KERN_INFO "MAP OK");
+	}
+	if (IS_ERR(from_user)) {
+		printk(KERN_ERR "Failed to get bpfmap, ERR %ld", PTR_ERR(from_user));
+		__bpfnic_helper_exit();
+		return PTR_ERR(from_user);
 	}
 
 	ret = register_switchdev_notifier(&bpfnic_switchdev_notifier);
 	if (ret) {
-		bpf_map_put(to_user);
-		/* bpf_map_put(from_user); */
+		__bpfnic_helper_exit();
 		printk(KERN_ERR "bpfnic - failed to register switchdev notifier\n");
 	} else {
 		init_done = true;
 	}
+
+	ringbuf_task = kthread_run(ringbuf_thread, NULL, "bpfnic-helper");
+
+	if (IS_ERR(ringbuf_task)) {
+		printk(KERN_ERR "Failed to run helper thread, ERR %ld", PTR_ERR(from_user));
+		__bpfnic_helper_exit();
+		return PTR_ERR(ringbuf_task);
+	}
+	
 	return ret;
 }
 
-
-static __exit void bpfnic_helper_exit(void)
+static void __bpfnic_helper_exit(void)
 {
+	exiting = true;
+	wmb();
 	if (init_done) {
 		unregister_switchdev_notifier(&bpfnic_switchdev_notifier);
 		init_done = false;
 	}
-	if (to_user)
+	if (to_user && (!IS_ERR(to_user)))
 		bpf_map_put(to_user);
+	if (from_user && (!IS_ERR(from_user))) {
+		from_user->ops->map_lookup_elem(NULL, NULL);
+		bpf_map_put(from_user);
+	}
+}
+
+static __exit void bpfnic_helper_exit(void)
+{
+	__bpfnic_helper_exit();
 }
 
 module_init(bpfnic_helper_init);
 module_exit(bpfnic_helper_exit);
 module_param(to_user_path, charp, S_IRUSR); 
+module_param(from_user_path, charp, S_IRUSR); 
 
 
 MODULE_VERSION("0.1.0");
--- setup_ringbuf.py
+++ setup_ringbuf.py
@@ -14,14 +14,15 @@
 
 
 from pybpfmap.bpfrecord import BPFMap
-from pybpfmap.map_types import BPF_MAP_TYPE_RINGBUF
+from pybpfmap.map_types import BPF_MAP_TYPE_RINGBUF, BPF_MAP_TYPE_USER_RINGBUF
 
 from os import unlink, chmod
 import sys
 import time
 from struct import Struct, calcsize
 
-PIN="/sys/fs/bpf/test_ringbuf"
+FROM_K="/sys/fs/bpf/test_ringbuf_from_k"
+TO_K="/sys/fs/bpf/test_ringbuf_to_k"
 
 #/* definition of a sample sent to user-space from BPF program */
 #struct event {
@@ -32,11 +33,11 @@ PIN="/sys/fs/bpf/test_ringbuf"
 
 PARSER_DEF = [("ifindex", "I"), ("addr",["B","B","B","B","B","B"]), ("added_by_user","B"), ("offloaded", "B"), ("vid", "H"), ("event", "H")]
 
-def setup_ringbuf(arg):
+def setup_ringbuf(arg, map_type):
     '''Set up a ringbuff for the kernel switchdev notifications'''
     m = BPFMap(
             -1,
-            BPF_MAP_TYPE_RINGBUF,
+            map_type,
             "rb".encode('ascii'),
             0,
             0,
@@ -54,14 +55,19 @@ def print_mac(arg):
         
 
 
-def get_events(m):
-    events = m.fetch_next(want_parsed=True)
+def get_events(inmap, outmap):
+    events = inmap.fetch_next(want_parsed=True)
+    print("Have {} events".format(len(events)))
     for event in events:
-        event["addr"] = print_mac(event["addr"])
+        event["print_addr"] = print_mac(event["addr"])
         print("{}".format(event))
+        event["offloaded"] = 0
+        outmap.submit(event)
+        
 
-m = setup_ringbuf(PIN)
+inmap = setup_ringbuf(FROM_K, BPF_MAP_TYPE_RINGBUF)
+outmap = setup_ringbuf(TO_K, BPF_MAP_TYPE_USER_RINGBUF)
 
 while True:
     time.sleep(1)
-    get_events(m)
+    get_events(inmap, outmap)
